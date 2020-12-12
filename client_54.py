c15d2e184de36e2d17bb497efeb7b052a79f9015
atikah <2020974753@isiswa.uitm.edu.my>
2020-12-12 22:29:13 +0800

    Self code 5.4

--- client_54.py
+++ client_54.py
@@ -1,18 +1,44 @@
 import socket
 import sys
-import json
+#import json
+import re
 
 file = open('file.txt', 'r')
 file1 = file.read()
-print(file1)
-sendData = json.dumps(file1)
+#print(file1)
+#filename = str(file1.encode)
+#sendData = json.dumps(file1)
 
 s = socket.socket()
 
 port = 8080
 s.connect(('192.168.0.103', port))
+'''
+print("Which file you want to sent?")
+filename = input("Enter filename:")
+print("Filename: " + filename)
 
-s.sendall(bytes(sendData,encoding="utf-8"))
+check = re.search(filename, file1)
+
+if check:
+	s.send(str.encode(filename))
+else:
+	message = "Error"
+	s.send(str.encode(message))
+'''
+#while True:
+s.send(str.encode(file1))
+#data = s.recv(4096)
+#print(data)
+
+
+#s.send(str.encode(file1))
+#file1 = file.read()
+#file.close()
+print("Done sending")
+
+#s.send(file1)
+#s.sendall(bytes(sendData,encoding="utf-8"))
 
 s.close()
 
